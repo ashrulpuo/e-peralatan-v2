3c095a13c7f199493156b0073adc8532d2b33ea2
Paul Victer <paulvicter@gmail.com>
2020-04-11 22:19:08 +0800

    update

--- resources/views/semakan.blade.php
+++ resources/views/semakan.blade.php
@@ -39,19 +39,6 @@
 								</div>
 							</div>
 							@endif
-						@else
-							<div class="col-md-12">
-								<div class="box box-default">
-									<!-- /.box-header -->
-									<div class="box-body">
-										<div class="alert alert-success alert-dismissible">
-											<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-											<h4><i class="icon fa fa-check"></i>No permohonan anda salah</h4>
-										</div>
-									</div>
-									<!-- /.box-body -->
-								</div>
-							</div>
 						@endif
 							<div class="row">
 								<div class="col-md-12">
